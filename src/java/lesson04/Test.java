1cd3bf9ef600edc60bee1c73264888a5808684dc
NuclEOS <sapegboldyr@gmail.com>
2016-09-04 12:59:28 +0300

    Lesson04 - Part2 commit

--- src/java/lesson04/Test.java
+++ src/java/lesson04/Test.java
@@ -4,4 +4,18 @@ package lesson04;
  * Created by nucleos on 04.09.16.
  */
 public class Test {
+    public static void main(String[] args) {
+        User user =
+                new User("NAME", "SURNAME", 100, "LOGIN", "PASS");
+        System.out.println(user);
+        System.out.println(user.hashCode());
+//        user.setName("Василий");
+//        user.setSurname("Васильев");
+        System.out.println(user.hashCode());
+        User user2 =
+                new User("NAME", "SURNAME", 100, "LOGIN", "PASS");
+        System.out.println(user.equals(user2));
+        System.out.println(user == user2);
+        System.out.println(user.hashCode() == user2.hashCode());
+    }
 }
--- src/java/lesson04/User.java
+++ src/java/lesson04/User.java
@@ -4,4 +4,53 @@ package lesson04;
  * Created by nucleos on 04.09.16.
  */
 public class User {
+
+    private String name;
+    private String surname;
+    private Integer age;
+    private String login;
+    private String password;
+
+    public User(String name, String surname, Integer age, String login, String password) {
+        this.name = name;
+        this.surname = surname;
+        this.age = age;
+        this.login = login;
+        this.password = password;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    @Override
+    public int hashCode() {
+        return (name.hashCode()
+                + surname.hashCode()
+                + login.hashCode()
+                + password.hashCode()) /** age*/;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        if (this == object) {
+            return true;
+        }
+
+        if (object instanceof User) {
+            User user = (User) object;
+            if (name.equals(user.name)
+                    && surname.equals(user.surname)
+                    && age == user.age
+                    && login.equals(user.login)
+                    && password.equals(user.password)) {
+                return true;
+            }
+        }
+        return false;
+    }
 }
